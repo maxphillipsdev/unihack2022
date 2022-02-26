377c28e8fae2989b9ae2e4f7d73b2193fc62f37b
Arthur Wang <ArthurWang2000@Hotmail.com>
2022-02-26 21:22:46 +1100

    Added layout for studentprofile page

--- frontend/pages/student-profile.tsx
+++ frontend/pages/student-profile.tsx
@@ -1,18 +1,43 @@
 import React from "react";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Box, Text } from "@chakra-ui/react";
 
 const Profile = () => {
   return (
     <>
-      Fk my ass
-      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-        <GridItem w="100%" h="10" bg="blue.500">
-          Test
+      <br />
+      <Grid templateColumns="repeat(2, 1fr)" gap={6} sx={{ mx: "6" }}>
+        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" w="100%" height="90%" p={4} color="white">
+            This is the Box
+          </Box>
+          <Text fontSize="md" textAlign="center">
+            Lesson History
+          </Text>
         </GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
+        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" w="100%" height="90%" p={4} color="white">
+            This is the Box
+          </Box>
+          <Text fontSize="md" textAlign="center">
+            Statistics Visualization #1
+          </Text>
+        </GridItem>
+        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" w="100%" height="90%" p={4} color="white">
+            This is the Box
+          </Box>
+          <Text fontSize="md" textAlign="center">
+            Statistics Visualization #2
+          </Text>
+        </GridItem>
+        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" w="100%" height="90%" p={4} color="white">
+            This is the Box
+          </Box>
+          <Text fontSize="md" textAlign="center">
+            Statistics Visualization #3
+          </Text>
+        </GridItem>
       </Grid>
     </>
   );
