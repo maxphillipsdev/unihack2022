164282ca1c0f7012db89e6969e98f3f6bb8fcf9e
Tim Zhang <timzhang3@gmail.com>
2022-02-26 20:53:49 +1100

    Add page transition links and basic layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,63 +1,73 @@
 import { Box, Container, Flex, HStack, Text } from "@chakra-ui/react";
 
 import type { NextPage } from "next";
+import Link from "next/link";
 import HeaderSection from "../components/HeaderSection";
+import LessonCard from "../components/LessonCard";
+import { Heading } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
-    <Flex
-      as="main"
-      flexDir="column"
-      width="100%"
-      justify="space-evenly"
-      height="100vh"
-    >
-      <Box as="section" id="student-profile">
-        <Flex justify="center" align="center">
-          <Text>Student Profile</Text>
-        </Flex>
+    <Flex as="main" flexDir="column" width="100%" height="100vh">
+      <Box as="section" id="student-profile" h="40%">
+        <Link href="/student-profile">
+          <Box
+            style={{
+              margin: "14px",
+              background: "tomato",
+              cursor: "pointer",
+              borderRadius: "3px",
+            }}
+            h="90%"
+          >
+            <Flex justify="center" align="center">
+              <Text>Student Profile</Text>
+            </Flex>
+          </Box>
+        </Link>
       </Box>
-      <HStack
-        as="section"
-        id="recommended-rail"
-        overflowX="scroll"
-        p={3}
-        bgColor="gray.400"
-      >
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-        <Container p={2} marginRight={3} border="1px solid black">
-          Recommended Rail
-        </Container>
-      </HStack>
-      <HStack as="section" id="lessons-rail">
-        <Container>Lessons Rail</Container>
-      </HStack>
+      <Box style={{ margin: "10px" }}>
+        <Heading size="lg" style={{ margin: "10px" }}>
+          Recommended
+        </Heading>
+        <HStack
+          as="section"
+          id="recommended-rail"
+          overflowX="scroll"
+          p={3}
+          bgColor="gray.400"
+          style={{
+            margin: "14px",
+            borderRadius: "3px",
+          }}
+        >
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+        </HStack>
+      </Box>
+      <Box style={{ margin: "10px" }}>
+        <Heading size="lg" style={{ margin: "10px" }}>
+          Numeracy
+        </Heading>
+        <HStack
+          as="section"
+          id="lessons-rail"
+          overflowX="scroll"
+          p={3}
+          bgColor="gray.400"
+          style={{
+            margin: "14px",
+            borderRadius: "3px",
+          }}
+        >
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+          <LessonCard title="Title here" slug="slug-here"></LessonCard>
+        </HStack>
+      </Box>
     </Flex>
   );
 };
