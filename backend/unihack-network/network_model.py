ef91e00381e78ac8687ea7422a955ef7c8460b22
Tim Zhang <timzhang3@gmail.com>
2022-02-26 14:35:26 +1100

    Add network starter

---
+++ backend/unihack-network/network_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class RecEngine(nn.Module):
+    def __init__(self, input_dim = 4, output_dim=2):
+        super(RecEngine, self).__init__()
+        self.hidden_layer = nn.Linear(input_dim, input_dim * 2)
+        self.output_layer = nn.Linear(input_dim * 2, output_dim)
+    
+    def forward(self, x):
+        x = self.hidden_layer(x)
+        x = torch.tanh(x)
+        x = self.output_layer(x)
+        return x
