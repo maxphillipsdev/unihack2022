e237dc23ff99f90d2cf0faf643211f0fd8904a0d
Arthur Wang <ArthurWang2000@Hotmail.com>
2022-02-27 11:18:19 +1100

    Polished charts

--- frontend/pages/student-profile.tsx
+++ frontend/pages/student-profile.tsx
@@ -10,35 +10,35 @@ const Profile = () => {
     <>
       <br />
       <Grid templateColumns="repeat(2, 1fr)" gap={6} sx={{ mx: "6" }}>
-        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
-          <Box bg="white" w="100%" height="90%" p={4} color="white">
+        <GridItem w="100%" h="43vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" rounded="sm" w="100%" height="93%" p={4} color="white">
             <StudentHistory />
           </Box>
-          <Text fontSize="md" textAlign="center">
+          <Text fontSize="lg" textAlign="center">
             Lesson History
           </Text>
         </GridItem>
-        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
-          <Box bg="white" w="100%" height="90%" p={4} color="white">
+        <GridItem w="100%" h="43vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" rounded="sm" w="100%" height="93%" p={4} color="white">
             <StudentRadialBar />
           </Box>
-          <Text fontSize="md" textAlign="center">
+          <Text fontSize="lg" textAlign="center">
             Statistics Visualization #1
           </Text>
         </GridItem>
-        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
-          <Box bg="white" w="100%" height="90%" p={4} color="white">
+        <GridItem w="100%" h="43vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" rounded="sm" w="100%" height="93%" p={4} color="white">
             <SubjectCompentencyChart />
           </Box>
-          <Text fontSize="md" textAlign="center">
+          <Text fontSize="lg" textAlign="center">
             Statistics Visualization #2
           </Text>
         </GridItem>
-        <GridItem w="100%" h="38vh" bg="gray.200" boxShadow="md">
-          <Box bg="white" w="100%" height="90%" p={4} color="white">
+        <GridItem w="100%" h="43vh" bg="gray.200" boxShadow="md">
+          <Box bg="white" rounded="sm" w="100%" height="93%" p={4} color="white">
             <RandomChart />
           </Box>
-          <Text fontSize="md" textAlign="center">
+          <Text fontSize="lg" textAlign="center">
             Statistics Visualization #3
           </Text>
         </GridItem>
